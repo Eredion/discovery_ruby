d97cc0eebd7cd78b07c269b076eb9d2086028b7f
Eredion <alvaro.segovia@hotmail.com>
2021-02-05 14:49:32 +0100

    Rb06 finished

--- rb06/ex04/methods_everywhere.rb
+++ rb06/ex04/methods_everywhere.rb
@@ -1,3 +1,28 @@
 #!/usr/bin/ruby
 
-def reduce 
+def reduce str
+  puts "#{str[0..7]}"
+end
+
+def agranda str
+  str2 = str.dup
+  while str2.length < 8
+    str2 << 'Z'
+  end
+  puts str2
+end
+
+if ARGV.length == 0
+  puts "none"
+else
+  for arg in ARGV
+    if arg.length > 8
+     reduce(arg)
+    elsif arg.length < 8
+      agranda(arg)
+    else if arg.length == 8
+     puts arg
+    end
+  end
+end
+end
---
+++ rb06/ex05/scope_that.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby 
+
+def add_one param
+  param += 1
+end
+
+i = 1
+puts i
+puts "El numero es: " + i.to_s
+puts "LLamo a la funcion"
+puts "El numero es: " + i.to_s
